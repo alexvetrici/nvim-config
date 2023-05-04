a6a270e1f66b135dd626d9882dd0a2b1c79129b3
alex <alexvetrici@gmail.com>
2023-05-04 19:48:21 +0200

    update colorscheme.lua to manage errors

--- lua/user/core/plugin_config/colorscheme.lua
+++ lua/user/core/plugin_config/colorscheme.lua
@@ -23,5 +23,12 @@ require("gruvbox").setup({
   transparent_mode = false,
 })
 
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+local colorscheme = 'gruvbox'
+vim.o.background = 'dark' -- or 'light' for light mode
+
+local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+
+if not status_ok then
+  vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+  return
+end
